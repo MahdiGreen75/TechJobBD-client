ecff073f69d67ab50e95adbf3dae06929f44be01
Ricky Ponting <qarimahdi217@gmail.com>
2023-11-07 17:41:00 +0000

    Implement all the applied jobs data for applied jobs route.

--- src/components/routes/AppliedJobs.jsx
+++ src/components/routes/AppliedJobs.jsx
@@ -1,9 +1,63 @@
+import { useContext, useEffect, useState } from "react";
+import TableRow from "../Tabs/TableRow/TableRow";
+import { AuthContext } from "../../providers/AuthProvider";
+
 
 
 const AppliedJobs = () => {
+    const {appliedJobs} = useContext(AuthContext);
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/all-jobs")
+            .then(res => res.json())
+            .then(data => {
+                const filtered = data.filter(item=>appliedJobs.includes(item.job_post_id))
+                setJobs(filtered);
+            })
+    }, [appliedJobs])
+
     return (
         <div>
-            <h1>This is applied jobs.</h1>
+
+            <div className="relative overflow-x-auto">
+                <table className="w-full text-sm text-left text-gray-500">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-200">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                Order
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Job Poster Name
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Job Title
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Job Posting Date
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Application Deadline
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Salary range
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Total Applicant's Number
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Job Status
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            jobs.map((job, index) => <TableRow key={job._id} {...job} index={index}></TableRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     );
 };
