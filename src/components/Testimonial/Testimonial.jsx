383af5f7ea880f3de0ee3724b6b3a9d4de8e30e3
Ricky Ponting <qarimahdi217@gmail.com>
2023-11-09 12:02:31 +0000

    Add a testimonial section with react fast marquee.

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -6,77 +6,77 @@ const Testimonial = () => {
     return (
         <div>
             <div className="text-center my-5">
-                <p className="font-semibold text-sm sm:text-base md:text-2xl ">Our Happy Clients</p>
+                <p className="font-semibold text-sm sm:text-base md:text-2xl ">Testimonial</p>
             </div>
             <Marquee pauseOnHover={true}>
                 <div className="flex flex-col justify-between items-center  p-5 border-2 rounded-md drop-shadow-2xl m-5 h-96">
                     <img src="https://i.ibb.co/k3xv55S/profile-Img-1.jpg" className="w-32 h-32 rounded-full object-cover object-center" />
-                    <span className="text-xl font-bold">Mr. Shah Rukh Khan</span>
-                    <span className="text-base font-semibold">Bollywood Actor</span>
+                    <span className="text-xl font-bold">Mr. Stuart Lee</span>
+                    <span className="text-base font-semibold">Brain Station 23</span>
                     <div className="flex justify-start w-full">
                         <FaQuoteLeft></FaQuoteLeft>
                     </div>
-                    <span className="w-52 text-center italic">Anytime I need any number of cars for filming, I get on time.</span>
+                    <span className="w-52 text-center italic">I got a SWE Job in Brain Station 23 from their recommendation.</span>
                     <div className="flex justify-end w-full">
                         <FaQuoteRight></FaQuoteRight>
                     </div>
                 </div>
                 <div className="flex flex-col justify-between items-center  p-5 border-2 rounded-md drop-shadow-2xl m-5 h-96">
                     <img src="https://i.ibb.co/CVTCm35/profile-Img-2.jpg" className="w-32 h-32 rounded-full object-cover object-center" />
-                    <span className="text-xl font-bold">Mrs. Putu Khatun</span>
-                    <span className="text-base font-semibold">Masterchef Australia</span>
+                    <span className="text-xl font-bold">Mrs. Shabana Begum</span>
+                    <span className="text-base font-semibold">Infosys ltd.</span>
                     <div className="flex justify-start w-full">
                         <FaQuoteLeft></FaQuoteLeft>
                     </div>
-                    <span className="w-52 text-center italic">They helpled me gift a perfect car to my ex-boyfriend.</span>
+                    <span className="w-52 text-center italic">FrontWorldSoft offer me with double sallery.</span>
                     <div className="flex justify-end w-full">
                         <FaQuoteRight></FaQuoteRight>
                     </div>
                 </div>
                 <div className="flex flex-col justify-between items-center  p-5 border-2 rounded-md drop-shadow-2xl m-5 h-96">
                     <img src="https://i.ibb.co/CsVsN29/profile-Img-3.jpg" className="w-32 h-32 rounded-full object-cover object-center" />
-                    <span className="text-xl font-bold">Mrs. Nargis Doctor</span>
-                    <span className="text-base font-semibold">Medicine Specialist</span>
+                    <span className="text-xl font-bold">Mrs. Hasina Akter</span>
+                    <span className="text-base font-semibold">Graphics Designer</span>
                     <div className="flex justify-start w-full">
                         <FaQuoteLeft></FaQuoteLeft>
                     </div>
-                    <span className="w-52 text-center italic">They are always ready for give any service.</span>
+                    <span className="w-52 text-center italic">I got a huge Freelance project from their recommendation.</span>
                     <div className="flex justify-end w-full">
                         <FaQuoteRight></FaQuoteRight>
                     </div>
                 </div>
                 <div className="flex flex-col justify-between items-center  p-5 border-2 rounded-md drop-shadow-2xl m-5 h-96">
                     <img src="https://i.ibb.co/PW0Y0ZT/profile-Img-4.jpg" className="w-32 h-32 rounded-full object-cover object-center" />
-                    <span className="text-xl font-bold">Mst. Bou Rani</span>
-                    <span className="text-base font-semibold">Housewife</span>
+                    <span className="text-xl font-bold">Mst. Emma Watson</span>
+                    <span className="text-base font-semibold">Sr. Software Engineer</span>
                     <div className="flex justify-start w-full">
                         <FaQuoteLeft></FaQuoteLeft>
                     </div>
-                    <span className="w-52 text-center italic">The person made this website, I am his wife.</span>
+                    <span className="w-52 text-center italic">I got recruited in Google Inc. india by their recommendation.</span>
                     <div className="flex justify-end w-full">
                         <FaQuoteRight></FaQuoteRight>
                     </div>
                 </div>
                 <div className="flex flex-col justify-between items-center  p-5 border-2 rounded-md drop-shadow-2xl m-5 h-96">
                     <img src="https://i.ibb.co/W0qv9GS/profile-Img-5.jpg" className="w-32 h-32 rounded-full object-cover object-center" />
-                    <span className="text-xl font-bold">Mr. Salman Khan</span>
-                    <span className="text-base font-semibold">Bollywood Actor</span>
+                    <span className="text-xl font-bold">Mr. Sam Altman</span>
+                    <span className="text-base font-semibold">Devops Engineer</span>
                     <div className="flex justify-start w-full">
                         <FaQuoteLeft></FaQuoteLeft>
                     </div>
-                    <span className="w-52 text-center italic">My dream was to collect more car than SRK. This company fulfills it.</span>
+                    <span className="w-52 text-center italic">Devops is my dream job. They fulfills it.</span>
                     <div className="flex justify-end w-full">
                         <FaQuoteRight></FaQuoteRight>
                     </div>
                 </div>
                 <div className="flex flex-col justify-between items-center  p-5 border-2 rounded-md drop-shadow-2xl m-5 h-96">
                     <img src="https://i.ibb.co/wQZTbqw/profile-Img-6.jpg" className="w-32 h-32 rounded-full object-cover object-center" />
-                    <span className="text-xl font-bold">Mr. Amir Khan</span>
-                    <span className="text-base font-semibold">Bollywood Actor</span>
+                    <span className="text-xl font-bold">Mr. Ack Rann</span>
+                    <span className="text-base font-semibold">Product Quality Manager</span>
                     <div className="flex justify-start w-full">
                         <FaQuoteLeft></FaQuoteLeft>
                     </div>
-                    <span className="w-52 text-center italic">They give me a lot of car to make Dhoom 2 with discount.</span>
+                    <span className="w-52 text-center italic">I got a job by their recommendation.</span>
                     <div className="flex justify-end w-full">
                         <FaQuoteRight></FaQuoteRight>
                     </div>
